6472832073371034ef5e5caae81ca0a4d8cb69be
Jack Evans <jackevans24@gmail.com>
2019-02-15 16:08:58 +0000

    Add List/Detail view; Extract details to their own component.

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Rice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Tomasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'BananaMan' },
+  { id: 17, name: 'Ryenamo' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Cornado' }
+];
