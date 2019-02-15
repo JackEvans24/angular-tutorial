cd2152472c77b2cf352203c359f4ceb154fd78ab
Jack Evans <jackevans24@gmail.com>
2019-02-15 16:39:33 +0000

    MessageService added; Messages component added; getHeroes made async.

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core'
+
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +12,23 @@ import { HEROES } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-  }
-
-  ngOnInit() {
+    this.heroService.heroSelected(hero);
   }
 
 }
